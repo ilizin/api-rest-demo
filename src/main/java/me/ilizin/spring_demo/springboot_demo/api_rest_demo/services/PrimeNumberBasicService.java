321b73fc78c788fb99705f17d6feba2e9d7cca69
ilizin <ilio.zincone@gmx.com>
2025-04-12 16:51:08 +0200

    fix comments

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_demo/services/PrimeNumberBasicService.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_demo/services/PrimeNumberBasicService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Primary
 @Service
 // This is not necessary because the scope is singleton by default
-// The prototype scope creates an object or each injection
+// The prototype scope creates an object for each injection
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class PrimeNumberBasicService implements IPrimeNumberService {
     @Override
