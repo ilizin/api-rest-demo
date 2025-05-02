20761d49f3e489bf8e40b72b8a742dba9c2a6fd5
ilizin <ilio.zincone@gmx.com>
2025-05-02 07:05:25 +0200

    Some comments added

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_demo/services/PalindromeIterativeService.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_demo/services/PalindromeIterativeService.java
@@ -5,7 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+/* The Lazy Initialization postpones the creation of the object until it is first requested. This is useful when
+  the object creation is resource-intensive or when the object is not immediately required during the application startup.
+  By default, Spring initializes all singleton beans eagerly at the application startup. */
 @Lazy
+/* The Service annotation is used to indicate that a class belongs to the service layer in an application.
+   The service layer typically contains the business logic of the application. The @Service annotation is a specialization
+   of the Component annotation, meaning that classes annotated with Service are automatically detected during classpath scanning.*/
 @Service
 public class PalindromeIterativeService implements IPalindromeService {
     private static final Logger logger = LoggerFactory.getLogger(PalindromeIterativeService.class);
