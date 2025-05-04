56b0c9ab57e7929e7f1b0a755f35ba3c10a248ce
ilizin <ilio.zincone@gmx.com>
2025-05-04 17:40:55 +0200

    Added a comment

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_demo/ApiRestDemoApplication.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_demo/ApiRestDemoApplication.java
@@ -3,6 +3,10 @@ package me.ilizin.spring_demo.springboot_demo.api_rest_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* It's a convenience annotation that adds all of the following:
+- Configuration
+- EnableAutoConfiguration
+- ComponentScan */
 @SpringBootApplication(
 		// This is not really necessary because spring starts scanning from the current package
 		scanBasePackages = {"me.ilizin.spring_demo.springboot_demo.api_rest_demo"}
