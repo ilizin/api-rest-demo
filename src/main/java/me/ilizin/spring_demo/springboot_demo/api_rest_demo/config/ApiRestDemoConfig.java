d52655f23ff64955a47fba14ed0a68a635273f77
ilizin <ilio.zincone@gmx.com>
2026-02-21 18:05:51 +0100

    Parse yaml file, improve swagger basic info

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_demo/config/ApiRestDemoConfig.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_demo/config/ApiRestDemoConfig.java
@@ -1,19 +1,25 @@
 package me.ilizin.spring_demo.springboot_demo.api_rest_demo.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import me.ilizin.spring_demo.springboot_demo.api_rest_demo.services.SqrtEroneService;
 import me.ilizin.spring_demo.springboot_demo.api_rest_demo.services.ISqrtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /* It's a class level annotation indicating that an object is a source of bean definitions, so a class marked with @Configuration
    declares beans through @Bean annotated methods and can also be used to define inter-bean dependencies. */
 @Configuration
-public class ApiRestDemoConfig {
+public class ApiRestDemoConfig implements WebMvcConfigurer {
 
     // The bean id is sqrtEroneService, use @Bean("alternativeBeanId") to specify a different bean id
     @Bean
@@ -22,20 +28,21 @@ public class ApiRestDemoConfig {
     }
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info()
-                        .version("1.0.0-SNAPSHOT")
-                        .title("Api rest with JPA demo")
-                        .description("A CRUD API rest with Jpa/hibernate (h2) demo project")
-                        .contact(new Contact()
-                                .name("Contact the developer")
-                                .url("https://github.com/ilizin"))
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("Find out more about Swagger")
-                        .url("https://swagger.io/"));
+    public OpenAPI customOpenAPI() throws IOException {
+        ClassPathResource resource = new ClassPathResource("api-rest-demo-swagger.yaml");
+        Path path = Paths.get(resource.getURI());
+        String content = new String(Files.readAllBytes(path));
+        SwaggerParseResult result = new OpenAPIV3Parser().readContents(content);
+        if (result.getMessages().isEmpty() && result.getOpenAPI() != null) {
+            return result.getOpenAPI();
+        } else {
+            throw new RuntimeException("Failed to parse OpenAPI definition: " + result.getMessages());
+        }
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api-docs/**").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/");
     }
 }
